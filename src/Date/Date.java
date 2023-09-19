d22e7278ae45bff2dfa2ec04d5b4914be5c7ef18
vudaohoanglong <vuhoanglong2408@gmail.com>
2023-09-19 09:13:48 +0700

    Date Example

---
+++ src/Date/Date.java
@@ -0,0 +1,15 @@
+package Date;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Date {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now(); // get current date
+        LocalTime time = LocalTime.now(); // get current time
+        LocalDateTime dateTime = LocalDateTime.now(); // get current date and time
+
+    }
+}
