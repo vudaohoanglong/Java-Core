4c6f8cbb522a93435da84d5022576a10e307e998
vudaohoanglong <vuhoanglong2408@gmail.com>
2023-09-20 11:10:27 +0700

    adding method and constructor to enum

--- src/Enum/Enum.java
+++ src/Enum/Enum.java
@@ -3,7 +3,7 @@ package Enum;
 public class Enum {
     public static void main(String[] args) {
         for (SEASON s : SEASON.values()) {
-            System.out.println(s);
+            System.out.println(s.getDuration());
         }
     }
 }
--- src/Enum/SEASON.java
+++ src/Enum/SEASON.java
@@ -1,8 +1,19 @@
 package Enum;
 
 public enum SEASON {
-    Winter,
-    Spring,
-    Summer,
-    Fall,
+    Winter(10,12),
+    Spring(1,3),
+    Summer(4,6),
+    Fall(7,9);
+
+    private int start;
+    private int end;
+
+    SEASON(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+    public String getDuration() {
+        return this + " from " + this.start + " to " + this.end;
+    }
 }
