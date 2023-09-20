f28687261d225bc02d99b7f24eefbb97c8567841
vudaohoanglong <vuhoanglong2408@gmail.com>
2023-09-20 08:06:43 +0700

    Add LinkedHashSet and TreeSet

---
+++ src/Set/LinkedHashSet.java
@@ -0,0 +1,7 @@
+package Set;
+
+public class LinkedHashSet {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Set/TreeSet.java
@@ -0,0 +1,7 @@
+package Set;
+
+public class TreeSet {
+    public static void main(String[] args) {
+        java.util.TreeSet treeSet = new java.util.TreeSet();
+    }
+}
