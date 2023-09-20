ed0f286642c916cdd21fbd0fec6994a2f3a14a61
vudaohoanglong <vuhoanglong2408@gmail.com>
2023-09-20 10:26:09 +0700

    update calendar example

--- src/Calendar/Calendar.java
+++ src/Calendar/Calendar.java
@@ -1,7 +1,15 @@
 package Calendar;
 
+import java.util.Date;
+
 public class Calendar {
     public static void main(String[] args) {
-
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+        Date date = calendar.getTime(); //
+        System.out.println("The current date is : " + date);
+        calendar.add(java.util.Calendar.DATE, -15);
+        System.out.println("15 days ago: " + calendar.getTime());
+        calendar.add(java.util.Calendar.MONTH,+4);
+        System.out.println("4 months later: " + calendar.get(java.util.Calendar.MONTH));
     }
 }
