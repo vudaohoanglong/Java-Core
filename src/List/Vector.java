b88212fd201d5c71973f550c45159b7c7c0c9ea9
vudaohoanglong <vuhoanglong2408@gmail.com>
2023-09-20 15:28:48 +0700

    update vector example

--- src/List/Vector.java
+++ src/List/Vector.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public class Vector {
     public static void main(String[] args) {
-        java.util.Vector<String> pokemonList = new java.util.Vector<>();
-        pokemonList.add("Pikachu");
-        System.out.println(pokemonList.get(0));
+        java.util.Vector<String> vector = new java.util.Vector<>();
+        vector.add("Hi");
+        vector.add("Hello");
+        vector.add("Bye");
+        System.out.println(vector.elementAt(2));
+        vector.insertElementAt("Good night",2);
+        System.out.println(vector);
+        vector.removeElementAt(0);
+        System.out.println(vector);
+        vector.setElementAt("Good morning",2);
+        System.out.println(vector);
     }
 }
